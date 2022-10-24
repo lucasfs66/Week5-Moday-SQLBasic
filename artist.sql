158d40323d4ec0fed3b5386df800be9288155cc6
Lucas <lucasfreiresabino@gmail.com>
2022-10-24 16:26:36 -0600

    finish the employee and artist

---
+++ artist.sql
@@ -0,0 +1,13 @@
+-- INSERT INTO artist (artist_id, name)
+-- VALUES(276, 'burrito'),
+-- (277, 'Pizza'),
+-- (278, 'burrito');
+
+
+-- SELECT * FROM artist ORDER BY name DESC LIMIT 10;
+
+-- SELECT * FROM artist ORDER BY name LIMIT 5;
+
+-- SELECT * FROM artist WHERE name LIKE 'Black%';
+
+-- SELECT * FROM artist WHERE name LIKE '%Black%';
